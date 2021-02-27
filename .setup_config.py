b37f986fdabc80a04e4bc29331fdfa1c82efabe0
Serhii Bura <serhiibura@gmail.com>
2021-02-27 20:43:25 +0200

    [refactoring] : Add build setup script
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

---
+++ .setup_config.py
@@ -0,0 +1,61 @@
+import os
+import glob
+
+#===============Config Settings =====================
+
+config_file = "BuildConfig.mk"
+
+CFG_PARAM_BOARD_FIELD = "BOARD"
+
+config_params = {
+    CFG_PARAM_BOARD_FIELD : None
+}
+
+#====================================================
+
+boards = ["stm32f103"]
+
+def board_select():
+    print("Please select the board:")
+
+    for board in boards:
+        print("\t{} - {}".format(boards.index(board), board))
+
+    board = int(input("Board: "))
+
+    if board < len(boards):
+        config_params[CFG_PARAM_BOARD_FIELD] = boards[board]
+
+        print(config_params)
+
+    else:
+        print("Error: invaid board id!")
+
+        y = input("Do you want reselect? y/n: ")
+
+        if "y" == y:
+            board_select()
+
+def change_config():
+    buffer = []
+    with open(config_file, "r") as config:
+        for line in config.readlines():
+            print(line)
+            config_param = line[:line.find("=")]
+            if config_param in config_params:
+                buffer.append("{}={}".format(config_param,
+                                             config_params[config_param]))
+            else:
+                buffer.append("{}=".format(config_param))
+
+    print(buffer)
+
+    with open(config_file, "w") as config:
+        for line in buffer:
+            config.write(line)
+            config.write("\n")
+
+if __name__ == "__main__":
+    board_select()
+    change_config()
+
